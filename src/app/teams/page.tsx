12f4e1095a3e1d43a8842fe312328c8dd64461c2
raihanfajar <rhnfjr@gmail.com>
2025-07-08 22:50:14 +0700

    perbaikan teams picture

--- src/app/teams/page.tsx
+++ src/app/teams/page.tsx
@@ -31,7 +31,7 @@ function Teams() {
   return (
     <section className="container mx-auto mt-[100px] px-4">
       <div className="flex min-h-screen flex-col items-center justify-center">
-        <h1 className="text-2xl font-bold md:text-5xl my-15">Meet Our Teams</h1>
+        <h1 className="my-15 text-2xl font-bold md:text-5xl">Meet Our Teams</h1>
         <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
           {teamMembers.map((member, index) => (
             <div
@@ -39,11 +39,10 @@ function Teams() {
               className="relative flex h-[290px] w-[320px] items-center justify-center md:w-[400px]"
             >
               <div className="absolute top-0 left-1/2 h-[80px] w-[80px] -translate-x-1/2 overflow-hidden rounded-full border-2 border-black md:h-[100px] md:w-[100px]">
-                <Image
+                <img
                   src={member.picture.large}
                   alt={`${member.name.first} ${member.name.last}`}
-                  fill
-                  className="object-cover"
+                  className="h-full w-full rounded-full object-cover"
                 />
               </div>
               <div className="mt-9 flex h-[calc(100%-72px)] w-full flex-col items-center justify-center rounded-2xl border-2 border-black bg-[#7bbcb6] px-5 text-[#03045E]">
